7e73493492ad2642cd603f13d789f85356c6b011
Anatoliy <strandpunk@yahoo.com>
2023-06-22 15:24:27 +0300

    header update

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css';
+
+
+
+function Header() {
+  return (
+    <div className="Header">
+      <div>
+        <img id='Logo' src="/1618916.svg" alt="Logo"/>
+      </div>
+      <div>
+        Grankov
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import './Main.css';
+
+
+function Main() {
+  return (
+    <div className="Main">
+      Main
+    </div>
+  );
+}
+
+export default Main;
