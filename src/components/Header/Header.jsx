deb3af07f2a640acaa5040876df5ca68a239794d
Anatoliy <strandpunk@yahoo.com>
2023-07-04 16:09:20 +0300

    header update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import './Header.css';
 
-
+let user = "Grankov";
 
 function Header() {
   return (
     <div className="Header">
       <div>
-        <img id='Logo' src="/1618916.svg" alt="Logo"/>
+        <img id='Logo' src="/1618916.svg" alt="Logo" />
       </div>
       <div>
-        Grankov
+        <p>User: {user}</p>
+      </div>
+      <div>
+        <button className='header__button'>Exit</button>
       </div>
     </div>
   );
