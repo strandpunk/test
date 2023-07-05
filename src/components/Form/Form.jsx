fad05f9e15259ca98724ea5e6276c10c63920e98
Anatoliy <strandpunk@yahoo.com>
2023-07-05 16:42:48 +0300

    password validation update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -10,6 +10,7 @@ function Form() {
     const [passwordDirty, setPasswordDirty] = useState(false);
     const [emailError, setEmailError] = useState('Поле почты не может быть пустым')
     const [passwordError, setPasswordError] = useState('Поле пароля не может быть пустым')
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
     const [formValid, setFormValid] = useState(false)
 
     useEffect(() => {
@@ -35,13 +36,34 @@ function Form() {
 
     const passwordHandler = (e) => {
         setPassword(e.target.value)
-        if (e.target.value.length < 4 || e.target.value.length > 8) {
-            setPasswordError('Пароль должен быть длиннее 4 и короче 8')
-            if (!e.target.value) {
-                setPasswordError('Поле пароля не может быть пустым')
-            }
+        const uppercaseRegExp = /(?=.*?[A-Z])/;
+        const lowercaseRegExp = /(?=.*?[a-z])/;
+        const digitsRegExp = /(?=.*?[0-9])/;
+        const specialCharRegExp = /(?=.*?[#?!@$%^&*-])/;
+        const minLengthRegExp = /.{8,}/;
+
+        const passwordLength = e.target.value.length;
+        const uppercasePassword = uppercaseRegExp.test(e.target.value);
+        const lowercasePassword = lowercaseRegExp.test(e.target.value);
+        const digitsPassword = digitsRegExp.test(e.target.value);
+        const specialCharPassword = specialCharRegExp.test(e.target.value);
+        const minLengthPassword = minLengthRegExp.test(e.target.value);
+
+
+        if (passwordLength === 0) {
+            setPasswordError('Password is empty');
+        } else if (!uppercasePassword) {
+            setPasswordError('At least one Uppercase');
+        } else if (!lowercasePassword) {
+            setPasswordError('At least one Lowercase');
+        } else if (!digitsPassword) {
+            setPasswordError('At least one digit');
+        } else if (!specialCharPassword) {
+            setPasswordError('At least one Special Characters');
+        } else if (!minLengthPassword) {
+            setPasswordError('At least minumum 8 characters');
         } else {
-            setPasswordError('')
+            setPasswordError('');
         }
     }
 
@@ -61,9 +83,13 @@ function Form() {
                 <form>
                     <h1>Registration</h1>
                     {(emailDirty && emailError) && <div style={{ color: 'red' }}>{emailError}</div>}
+                    <label>Email</label>
                     <input onChange={e => emailHandler(e)} value={email} onBlur={e => blurHandler(e)} name='email' type='text' placeholder='Enter your email....' />
                     {(passwordDirty && passwordError) && <div style={{ color: 'red' }}>{passwordError}</div>}
+                    <label>Password</label>
                     <input onChange={e => passwordHandler(e)} value={password} onBlur={e => blurHandler(e)} name='password' type='password' placeholder='Enter your password....' />
+                    <label>Confirm password</label>
+                    <input type='password' placeholder='Confirm your password...'></input>
                     <button className='registerbtn' disabled={!formValid} type='submit'>Registration</button>
                 </form>
             </div>
