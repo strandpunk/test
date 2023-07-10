16fcd2f1b1bfce376e725d6b7f4f1da85384abe8
Anatoliy <strandpunk@yahoo.com>
2023-07-10 14:21:00 +0300

    server full update

--- server/index.js
+++ server/index.js
@@ -1,14 +1,43 @@
-import express from 'express'
-
-const PORT = 4000;
+const { randomBytes } = require('crypto')
+const express = require('express')
 
 const app = express()
-
 app.use(express.json())
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).json('Сервер работает')
+PORT = 4000
+
+let users = [
+    {
+        id: "1",
+        name: 'Anna'
+    }
+]
+
+app.get('/users', (_req, res) => {
+    res.send(users)
 })
 
-app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
+app.post('/users', (req, res) => {
+    const new_user_id = randomBytes(4).toString('hex')
+    const new_user = {
+        id: new_user_id,
+        name: req.body.name
+    }
+    users.push(new_user)
+    res.send(new_user)
+})
+
+app.put('/users/:id', (req, res) => {
+    const user_id = req.params.id
+    const user = users.find(u => u.id === user_id)
+    user.name = req.body.name
+    res.send(user)
+})
+
+app.delete('/users/:id', (req, res) => {
+    const user_id = req.params.id
+    users = users.filter(u => user_id !== u.id)
+    res.send('user ' + user_id + ' was deleted')
+})
+
+app.listen(PORT, () => console.log('SERVER IS RUNNING ON PORT: ' + PORT))
