b8509cc3be2ac61c8b57dede90d060068095e83e
Anatoliy <strandpunk@yahoo.com>
2023-07-11 15:32:20 +0300

    asyncMiddleware added

--- server/services/users/users.controller.js
+++ server/services/users/users.controller.js
@@ -1,55 +1,28 @@
+const asyncMiddleware = require('../../middleware/asyncMiddleware')
 const User = require('./users.model')
 
-const getUsers = async (_req, res) => {
-    try {
-        const users = await User.find()
-        res.status(200).send(users)
-    }
-    catch (error) {
-        console.status(500).send(error.message)
-    }
+const getUsers = asyncMiddleware(async (_req, res) => {
+    const user = await User.find()
+    res.status(200).send(users)
+})
 
-}
+const addUsers = asyncMiddleware(async (_req, res) => {
+    if (_req.body?.name === '') throw new Error("Name is required")
+    const new_user = await User.create(_req.body)
+    res.status(201).send(new_user)
+})
 
-const addUsers = async (req, res) => {
-    try {
-        if (req.body?.name === '') {
-            res.status(404).send("Name is equired")
-            return
-        }
-        const new_user = await User.create(req.body)
-        res.status(201).send(new_user)
-    }
-    catch (error) {
-        console.status(500).send(error.message)
-    }
-}
+const updateUsers = asyncMiddleware(async (_req, res) => {
+    if (req.body?.name === '') throw new Error("Name is required")
+    const updated_user = await User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    res.status(200).send(updated_user)
+})
 
-const updateUsers = async (req, res) => {
-    try {
-        if (req.body?.name === '') {
-            res.status(404).send("Name is equired")
-            return
-        }
-        console.log(req.params.id)
-        const updated_user = await User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-        res.status(200).send(updated_user)
-    }
-    catch (error) {
-        console.status(500).send(error.message)
-    }
-}
-
-const deleteUsers = async (req, res) => {
-    try {
-        const user_id = req.params.id
-        await User.findByIdAndRemove(user_id)
-        res.status(204).end()
-    }
-    catch (error) {
-        console.status(500).send(error.message)
-    }
-}
+const deleteUsers = asyncMiddleware(async (_req, res) => {
+    const user_id = req.params.id
+    await User.findByIdAndRemove(user_id)
+    res.status(204).end()
+})
 
 module.exports = {
     getUsers,
