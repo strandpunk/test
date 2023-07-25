15ee8baf80e7d9075b9559a30b74de5ffbcc1e4c
Anatoliy <strandpunk@yahoo.com>
2023-07-25 13:04:57 +0300

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import AuthContextProvider from './contexts/AuthContext';
 import DataContextProvider from './contexts/DataContext';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
-import SignUp from './pages/signUp';
+import SignUp from './pages/SignUp';
 import React from 'react'
 import './App.css'
 import Layout from './templates/Layout';
